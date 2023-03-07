0430a1da82833dca6eababaab90af6c156fbcdb7
ilyes-guy <random_dude_233@proton.me>
2023-03-07 17:31:48 +0100

    minimalist cards

--- src/routes/game/minimal/+page.svelte
+++ src/routes/game/minimal/+page.svelte
@@ -9,6 +9,26 @@
 
 
 
+    <div class='black_card  rounded-lg w-32 h-48 cursor-pointer relative flex items-end justify-center' draggable="true" >
+		<div class='absolute w-24 h-16 rounded-[50%] border-4 border-red-600  top-1/2 -translate-y-1/2 -rotate-12  flex items-center justify-center'>
+			<div class='text-[45px]  minimal_uno_text  font-semibold   text-white  text-yellow-300 '  >
+				UNO
+			</div>
+		</div>
+		<div class="bg-white h-1 w-full mb-4 flex  ">
+			<div class='h-full w-1/4 bg-red-600' ></div>
+			<div class='h-full w-1/4 bg-green-600' ></div>
+			<div class='h-full w-1/4 bg-blue-600' ></div>
+			<div class='h-full w-1/4 bg-yellow-400' ></div>
+		</div>
+	</div>
+
+
+
+
+
+
+
     <div class='black_card p-1 rounded-lg w-32 h-48 cursor-pointer' draggable="true" >
 		<div class={'w-full h-full  rounded-lg  flex flex-col items-center justify-center  '}>
 			<div class="w-full flex items-start justify-start h-6 p-1 ">
@@ -51,10 +71,10 @@
 			</div>
 			<div class=' w-full h-3/4  flex items-center justify-center'>
 				<div class="absolute  w-8 h-12  ">
-                    <div class="absolute w-full h-full  rounded-md  top-8 right-8    border-2 border-green-600"></div>
+                    <div class="absolute w-full h-full  rounded-md  top-6 right-6    border-2 border-green-600"></div>
                     <div class="absolute w-full h-full  rounded-md  bottom-8 left-8  border-2 border-red-600"></div>
                     <div class="absolute w-full h-full  rounded-md  bottom-4 right-2 border-2 border-yellow-400"></div>
-                    <div class="absolute w-full h-full  rounded-md  top-4 left-2     border-2 border-blue-600"></div>
+                    <div class="absolute w-full h-full  rounded-md  top-2 left-4     border-2 border-blue-600"></div>
                 </div>
 			</div>
 			<div class="w-full flex items-start justify-end h-6 pr-1">
@@ -167,6 +187,121 @@
 			</div>
 		</div>
 	</div>
+
+
+
+
+
+
+
+
+
+
+
+
+    <div class='yellow_card p-1 rounded-lg w-32 h-48 cursor-pointer' draggable="true" >
+		<div class={'w-full h-full  rounded-lg  flex flex-col items-center justify-center  '}>
+			<div class="w-full flex items-start justify-start h-6 p-1">
+				<div class=' w-6 h-6 relative flex items-center justify-center'>
+					<div class='absolute w-6 h-6 rounded-[50%] border-2'></div>
+					<div class='absolute h-6  border-[1px] -rotate-45  bg-white '></div>
+				</div>
+			</div>
+			<div class=' w-full h-3/4  flex items-center justify-center relative'>
+                <div class='absolute w-16 h-16 rounded-[50%] border-4'></div>
+                <div class='absolute h-16 w-1 -rotate-45 bg-white '></div>
+			</div>
+			<div class="w-full flex items-end justify-end h-6 p-1 ">
+				<div class=' w-6 h-6 relative flex items-center justify-center'>
+					<div class='absolute w-6 h-6 rounded-[50%] border-2'></div>
+					<div class='absolute h-6  border-[1px] -rotate-45  bg-white '></div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <div class='yellow_card p-1 rounded-lg w-32 h-48 cursor-pointer' draggable="true" >
+		<div class={'w-full h-full  rounded-lg  flex flex-col items-center justify-center  '}>
+			<div class="w-full flex items-start justify-start h-6   ">
+				<div class='w-6 h-6 mt-3'>
+					<div class='absolute w-6 h-2 rounded-br-md border-b-2 border-r-2 mb-2 ml-1 rotate-[135deg]  '>
+						<div class='w-2 h-2 border-b-2 border-l-2   absolute top-[3px]  rotate-45'></div>
+					</div>
+					<div class='absolute w-6 h-2 rounded-br-md  border-b-2 border-r-2 mt-2 mr-1 -rotate-45  '>
+						<div class='w-2 h-2 border-b-2 border-l-2   absolute top-[3px]  rotate-45'></div>
+					</div>
+				</div>
+			</div>
+			<div class=' w-full h-3/4  flex items-center justify-center relative'>
+                <div class='absolute w-12 h-4 rounded-br-xl border-b-2 border-r-2 mb-4 ml-2 rotate-[135deg]  '>
+					<div class='w-4 h-4 border-b-2 border-l-2   absolute top-[7px]  rotate-45'></div>
+				</div>
+                <div class='absolute w-12 h-4 rounded-br-xl  border-b-2 border-r-2 mt-4 mr-2 -rotate-45  '>
+					<div class='w-4 h-4 border-b-2 border-l-2   absolute top-[7px]  rotate-45'></div>
+				</div>
+			</div>
+			<div class="w-full flex items-start justify-end h-6 pr-1">
+				<div class='w-6 h-6 mb-3'>
+					<div class='absolute w-6 h-2 rounded-br-md border-b-2 border-r-2 mb-2 ml-1 rotate-[135deg]  '>
+						<div class='w-2 h-2 border-b-2 border-l-2   absolute top-[3px]  rotate-45'></div>
+					</div>
+					<div class='absolute w-6 h-2 rounded-br-md  border-b-2 border-r-2 mt-2 mr-1 -rotate-45  '>
+						<div class='w-2 h-2 border-b-2 border-l-2   absolute top-[3px]  rotate-45'></div>
+					</div>
+				</div>	
+			</div>
+		</div>
+	</div>
+
+
+
+    <div class='red_card p-1 rounded-lg w-32 h-48 cursor-pointer' draggable="true" >
+		<div class={'w-full h-full  rounded-lg  flex flex-col items-center justify-center  '}>
+			<div class="w-full flex items-start justify-start h-6   ">
+				<div class='w-6 h-6 mt-3'>
+					<div class='absolute w-6 h-2 rounded-br-md border-b-2 border-r-2 mb-2 ml-1 rotate-[135deg]  '>
+						<div class='w-2 h-2 border-b-2 border-l-2   absolute top-[3px]  rotate-45'></div>
+					</div>
+					<div class='absolute w-6 h-2 rounded-br-md  border-b-2 border-r-2 mt-2 mr-1 -rotate-45  '>
+						<div class='w-2 h-2 border-b-2 border-l-2   absolute top-[3px]  rotate-45'></div>
+					</div>
+				</div>
+			</div>
+			<div class=' w-full h-3/4  flex items-center justify-center relative'>
+                <div class='absolute w-12 h-4 rounded-br-xl border-b-2 border-r-2 mb-4 ml-2 rotate-[135deg]  '>
+					<div class='w-4 h-4 border-b-2 border-l-2   absolute top-[7px]  rotate-45'></div>
+				</div>
+                <div class='absolute w-12 h-4 rounded-br-xl  border-b-2 border-r-2 mt-4 mr-2 -rotate-45  '>
+					<div class='w-4 h-4 border-b-2 border-l-2   absolute top-[7px]  rotate-45'></div>
+				</div>
+			</div>
+			<div class="w-full flex items-start justify-end h-6 pr-1">
+				<div class='w-6 h-6 mb-3'>
+					<div class='absolute w-6 h-2 rounded-br-md border-b-2 border-r-2 mb-2 ml-1 rotate-[135deg]  '>
+						<div class='w-2 h-2 border-b-2 border-l-2   absolute top-[3px]  rotate-45'></div>
+					</div>
+					<div class='absolute w-6 h-2 rounded-br-md  border-b-2 border-r-2 mt-2 mr-1 -rotate-45  '>
+						<div class='w-2 h-2 border-b-2 border-l-2   absolute top-[3px]  rotate-45'></div>
+					</div>
+				</div>	
+			</div>
+		</div>
+	</div>
+
+
+
 
 
 </div>
