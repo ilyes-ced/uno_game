d10fb1cf3904904f96e30cec506d49ad8358e433
ilyes-guy <random_dude_233@proton.me>
2023-03-21 20:06:42 +0100

    idk

--- app/src/routes/Home_page.svelte
+++ app/src/routes/Home_page.svelte
@@ -1,10 +1,10 @@
-<div class="h-full w-fit bg-[#100a29] mx-auto">
+<div class="h-full w-fit bg-[#100a29] mx-auto max-w-[1200px]">
 
     <div>
-        <img src="../../../../../lib/images/uno_game.jpg" alt="dd">
+        <img src="../images/purple_uno_img.jpg" alt="dd">
     </div>
 
-    <div class='w-full bg-red-900 flex space-x-24 px-24'>
+    <div class='w-full  flex space-x-24 px-24'>
         <div class='rotate-[20deg] -skew-y-[20deg] w-1/3 h-64 bg-[#28233f] '>
             play vs ai
         </div>
